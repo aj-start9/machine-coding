256a72796195d3bfc5bf05a988b6c8128acd8c3b
one-ankit <ankit.jain@onepercentclub.com>
2025-05-13 11:47:49 +0530

    add dymiac route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,18 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
+
+  const list = [
+    {
+      name: "Interactive Diagonal",
+      link: 'interactive-diagonal'
+    },
+    {
+      name: 'Nested Checkbox',
+      link: 'nested-checkbox'
+    }
+  ]
+
   return (
     <div className="min-h-screen flex flex-col p-6 sm:p-10 bg-gray-50 dark:bg-black text-gray-900 dark:text-white font-sans">
       {/* Header */}
@@ -21,27 +33,27 @@ export default function Home() {
       </header>
       <main className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 flex-1">
         {/* Example Project Card */}
-        <Link
-          href="/interactive-diagonal"
-          className="h-28 group p-4 rounded-lg border border-gray-200 dark:border-gray-800 bg-white dark:bg-[#111] shadow-sm hover:shadow-md transition hover:scale-[1.02] flex flex-col items-center text-center"
-        >
-          <div className="rounded-md bg-gray-100 dark:bg-gray-900 p-2 mb-2">
-            <Image
-              src="/file.svg"
-              alt="Project Icon"
-              width={24}
-              height={24}
-              className="group-hover:rotate-6 transition"
-            />
-          </div>
-          <h2 className="text-sm font-semibold group-hover:text-primary">Interactive Diagonal</h2>
-        </Link>
-        <Link href="/nested-checkbox" className="h-28 group p-4 rounded-lg border border-gray-200 dark:border-gray-800 bg-white dark:bg-[#111] shadow-sm hover:shadow-md transition hover:scale-[1.02] flex flex-col items-center text-center">
-          <div className="rounded-md bg-gray-100 dark:bg-gray-900 p-2 mb-2">
-            <Image src="/file.svg" alt="Project Icon" width={24} height={24} className="group-hover:rotate-6 transition" />
-          </div>
-          <h2 className="text-sm font-semibold group-hover:text-primary">Nested Checkbox</h2>
-        </Link>
+        {
+          list.map((itm: any) => {
+            return (
+              <Link
+                href={`/${itm?.link}`}
+                className="h-28 group p-4 rounded-lg border border-gray-200 dark:border-gray-800 bg-white dark:bg-[#111] shadow-sm hover:shadow-md transition hover:scale-[1.02] flex flex-col items-center text-center"
+              >
+                <div className="rounded-md bg-gray-100 dark:bg-gray-900 p-2 mb-2">
+                  <Image
+                    src="/file.svg"
+                    alt="Project Icon"
+                    width={24}
+                    height={24}
+                    className="group-hover:rotate-6 transition"
+                  />
+                </div>
+                <h2 className="text-sm font-semibold group-hover:text-primary">{itm?.name}</h2>
+              </Link>
+            )
+          })
+        }
       </main>
     </div>
   );
